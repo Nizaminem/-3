cfcc13df936ec2842568632a289b9c35607c70b0
Nizaminem <nagarizaev1994@gmail.com>
2024-12-12 02:38:06 +0300

    Дополнительное практическое задание модуль 3

--- Recursion.py
+++ Recursion.py
@@ -1,13 +1,13 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if str_number[-1] == '0':
-        str_number = str_number[:-1]
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    if first != 0:
         return first
+    return 1
+
 result = get_multiplied_digits(40203)
 print(result)
-result2 = get_multiplied_digits(402030)
+result2 = get_multiplied_digits(40203000)
 print(result2)
